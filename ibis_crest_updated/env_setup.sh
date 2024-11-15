96733f2e6ee2b6aa535d4857ada47e2710e9d462
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2024-11-15 15:40:50 -0700

    Changed in response to error for local Mac

--- ibis_crest_updated/env_setup.sh
+++ ibis_crest_updated/env_setup.sh
@@ -1,4 +1,4 @@
-conda create -n crest_env python=3.8
-conda activate crest_env
+mamba create -n crest_env python=3.8
+mamba activate crest_env
 
-conda install scikit-learn=0.23.2 numpy=1.19.2 pandas=1.1.3
+mamba install scikit-learn=0.23.2 numpy=1.19.4 pandas=1.1.3
